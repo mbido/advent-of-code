9b62516dc6249103e603a2b88f3544d90fbd1c7f
mbido <ma.bidault39@gmail.com>
2024-03-14 13:14:39 +0100

    adding day 4 of aoc 2015

---
+++ sources/2015/day-04.cr
@@ -0,0 +1,26 @@
+require "../utiles"
+
+data = get_input(2015, 4)
+
+def helper(data, prefix)
+  i = 0
+  while true
+    if Digest::MD5.hexdigest("#{data}#{i}").starts_with?(prefix)
+      return i
+    end
+    i += 1
+  end
+  -1
+end
+
+def part1(data)
+  helper(data, "00000")
+end
+
+puts part1 data
+
+def part2(data)
+  helper(data, "000000")
+end
+
+puts part2 data
--- sources/utiles.cr
+++ sources/utiles.cr
@@ -1,3 +1,5 @@
+require "digest"
+
 def get_input(year : Int32, day_number : Int32) : String
   add = day_number < 10 ? "0" : ""
   File.read("../../data/#{year}/day-#{add}#{day_number}.txt")
