13b277e52447bc75b5c0340c8faec1006bac401d
mbido <ma.bidault39@gmail.com>
2024-04-19 12:17:35 +0200

    aoc 2015 day 5

--- aoc_2015/src/day_05.py
+++ aoc_2015/src/day_05.py
@@ -6,17 +6,25 @@ from utils import *
 
 YEAR = 2015
 DAY = 5
-
-
-
-
+    
 def part_1(data):
-    return ""
+    counter = 0
+    for line in data.split("\n"):
+        nbOfVowels = len([1 for char in line if char in "aeiou"])
+        nbOfDouble = len([1 for index in range(len(line) - 1) if line[index] == line[index + 1]])
+        nbOfWrongFollow = len([1 for index in range(len(line) - 1) if line[index] + line[index + 1] in ["ab", "cd", "pq", "xy"]])
+        if nbOfVowels > 2 and nbOfDouble > 0 and nbOfWrongFollow == 0:
+            counter += 1
+    return counter
 
 def part_2(data):
-    return ""
-
-
+    counter = 0
+    for line in data.split("\n"):
+        nbOfCouple = len([1 for i in range(len(line) - 1) if line[i] + line[i + 1] in line[i + 2::]])
+        nbOfSeparatedByOne = len([1 for i in range(len(line) - 2) if line[i] == line[i + 2]])
+        if nbOfCouple > 0 and nbOfSeparatedByOne > 0:
+            counter += 1
+    return counter
 
 if __name__ == "__main__": 
     data = aoct.get_input(YEAR, DAY)
