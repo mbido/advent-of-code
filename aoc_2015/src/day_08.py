a11e27a6676f17771a79e4bbe2dbb02f9abfb4a6
mbido <ma.bidault39@gmail.com>
2024-04-19 18:43:38 +0200

    aoc 2015 day 8

--- aoc_2015/src/day_08.py
+++ aoc_2015/src/day_08.py
@@ -7,18 +7,21 @@ from utils import *
 YEAR = 2015
 DAY = 8
 
-
-
-
 def part_1(data):
-    return ""
+    res = 0
+    for line in data.split("\n"):
+        realSize = len(re.sub(r'(\\\\)|(\\\")|(\\x.{2})', "!", line)) - 2
+        res += len(line) - realSize
+    return res
 
 def part_2(data):
-    return ""
+    res = 0
+    for line in data.split("\n"):
+        newSize = len(re.sub(r'\"|\\', "!!", line)) + 2
+        res += newSize - len(line)
+    return res
 
-
-
-if __name__ == "__main__": 
+if __name__ == "__main__":
     data = aoct.get_input(YEAR, DAY)
     aoct.submit_answer(YEAR, DAY, part_1(data), send=False)
     aoct.submit_answer(YEAR, DAY, part_2(data), level=2, send=False)
