34042422cb2fb53ba89c8bb1985840575c58d278
mbido <ma.bidault39@gmail.com>
2024-04-21 14:38:05 +0200

    aoc 2015 day 11

--- aoc_2015/src/day_11.py
+++ aoc_2015/src/day_11.py
@@ -7,16 +7,41 @@ from utils import *
 YEAR = 2015
 DAY = 11
 
+def increments_string(string):
+    string = list(string)
+    for i in range(len(string) - 1, -1, -1):
+        letter = string[i] 
+        if letter != "z":
+            string[i] = chr(ord(letter) + 1)
+            return ''.join(string)
+        string[i] = "a"
+    return ''.join(string)
 
-
+def check_pass(string):
+    check = False
+    for i in range(len(string) - 2):
+        if string[i] == chr(ord(string[i + 1]) - 1) == chr(ord(string[i + 2]) - 2):
+            check = True
+            break
+    if not check:
+        return False
+    if re.search(r'[iol]', string) != None:
+        return False
+    if re.search(r'(.)\1.*([^\1])\2', string) == None:
+        return False
+    return True
 
 def part_1(data):
-    return ""
+    password = increments_string(data)
+    while not check_pass(password):
+        password = increments_string(password)
+    return password
 
 def part_2(data):
-    return ""
-
-
+    password = increments_string(part_1(data))
+    while not check_pass(password):
+        password = increments_string(password)
+    return password
 
 if __name__ == "__main__": 
     data = aoct.get_input(YEAR, DAY)
