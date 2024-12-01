6622ab3be5381c84e16c3b5c2f7743d37ade0a64
mbido <ma.bidault39@gmail.com>
2024-12-01 20:06:03 +0100

    2022 day 1

--- aoc_2022/src/day_01.py
+++ aoc_2022/src/day_01.py
@@ -8,10 +8,20 @@ YEAR = 2022
 DAY = 1
 
 def part_1(data):
-    return ""
+    elves = data.split("\n\n")
+    res = []
+    for i in range(len(elves)):
+        res.append(sum(int_lines(elves[i])))
+    res.sort(reverse=True)
+    return res[0]
 
 def part_2(data):
-    return ""
+    elves = data.split("\n\n")
+    res = []
+    for i in range(len(elves)):
+        res.append(sum(int_lines(elves[i])))
+    res.sort(reverse=True)
+    return sum(res[:3])
 
 if __name__ == "__main__": 
     data = aoct.get_input(YEAR, DAY)
