ca77b64d6fb140c102b1907e013857442521aba4
mbido <ma.bidault39@gmail.com>
2024-12-01 19:42:53 +0100

    2020 day 1

--- aoc_2020/src/day_01.py
+++ aoc_2020/src/day_01.py
@@ -8,10 +8,21 @@ YEAR = 2020
 DAY = 1
 
 def part_1(data):
-    return ""
+    d = [int(elt) for elt in data.split("\n")]
+    for i in range(len(d)):
+        for j in range(i, len(d)):
+            if d[i] + d[j] == 2020:
+                return d[i] * d[j]
+    return "ERROR"
 
 def part_2(data):
-    return ""
+    d = [int(elt) for elt in data.split("\n")]
+    for i in range(len(d)):
+        for j in range(i, len(d)):
+            for k in range(j, len(d)):
+                if d[i] + d[j] + d[k] == 2020:
+                    return d[i] * d[j] * d[k]
+    return "ERROR"
 
 if __name__ == "__main__": 
     data = aoct.get_input(YEAR, DAY)
