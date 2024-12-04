0a80f0435120ac5f378975c576ad370f012e0325
mbido <ma.bidault39@gmail.com>
2024-12-04 06:28:24 +0100

    2024 day 4

--- aoc_2024/src/day_04.py
+++ aoc_2024/src/day_04.py
@@ -7,11 +7,49 @@ from utils import *
 YEAR = 2024
 DAY = 4
 
+def get(x, y, grid):
+    if 0 <= x < len(grid[0]) and 0 <= y < len(grid):
+        return grid[y][x]
+    return "?"
+
+def check(x, x_sign, y, y_sign, grid):
+    m = "XMAS"
+    for i in range(len(m)):
+        if get(x + x_sign * i, y + y_sign * i, grid) != m [i]:
+            return False
+    return True
+
+def count_XMAS(x, y, grid):
+    res = []
+    signs = [-1, +1, 0]
+    for x_sign in signs:
+        for y_sign in signs:
+            res.append(check(x, x_sign, y, y_sign, grid))
+    return sum(res)
+
 def part_1(data):
-    return ""
+    res = 0
+    grid = data.split("\n")
+    for y in range(len(grid)):
+        for x in range(len(grid[0])):
+            if grid[y][x] == "X":
+                res += count_XMAS(x, y, grid)
+    return res
+
+def is_X_MAS(x, y, grid):
+    ms = ["MAS", "SAM"]
+    a = get(x- 1, y - 1, grid) + get(x, y, grid) + get(x + 1, y + 1, grid)
+    b = get(x + 1, y - 1, grid) + get(x, y, grid) + get(x - 1, y + 1, grid)
+    return a in ms and b in ms
 
 def part_2(data):
-    return ""
+    res = []
+    grid = data.split("\n")
+    for y in range(len(grid)):
+        for x in range(len(grid[0])):
+            if grid[y][x] == "A":
+                res.append(is_X_MAS(x, y, grid))
+    return sum(res)
 
 if __name__ == "__main__": 
     data = aoct.get_input(YEAR, DAY)
