57be8a42e6899f914f9d0163d5e133c30800f43e
mbido <ma.bidault39@gmail.com>
2024-12-07 14:07:50 +0100

    2022 day 2

--- aoc_2022/src/day_02.py
+++ aoc_2022/src/day_02.py
@@ -8,10 +8,33 @@ YEAR = 2022
 DAY = 2
 
 def part_1(data):
-    return ""
+    res = 0
+    for line in data.split("\n"):
+        a, b = line.split(" ")
+        a = ord(a) - ord("A") + 1
+        b = ord(b) - ord("X") + 1
+        if a == b:
+            res += b + 3
+        elif b == a % 3 + 1:
+            res += b + 6
+        else:
+            res += b
+    return res
 
 def part_2(data):
-    return ""
+    res = 0
+    for line in data.split("\n"):
+        a, b = line.split(" ")
+        a = ord(a) - ord("A") + 1
+        win = a % 3 + 1
+        lose = (a + 1) % 3 + 1
+        if b == "Y":
+            res += a + 3
+        elif b == "X":
+            res += lose
+        else:
+            res += win + 6
+    return res
 
 if __name__ == "__main__": 
     data = aoct.get_input(YEAR, DAY)
