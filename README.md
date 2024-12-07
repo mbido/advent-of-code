11de3d2f4c832983d4cfe4afabc81056982b1f5a
mbido <ma.bidault39@gmail.com>
2024-12-07 06:20:51 +0100

    2024 day 7

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 My participation at the advent of code in python
 
-![Statistiques AOC](https://aoc-stats.vercel.app/api/card?username=mbido&totalStars=112&currentYearStars=12&currentDay=6&completedDays=6&currentYear=2024)
+![Statistiques AOC](https://aoc-stats.vercel.app/api/card?username=mbido&totalStars=114&currentYearStars=14&currentDay76&completedDays=7&currentYear=2024)
--- aoc_2024/src/day_07.py
+++ aoc_2024/src/day_07.py
@@ -3,15 +3,49 @@ import sys
 sys.path.append(os.path.abspath(os.path.join(__file__, '..', '..', '..')))
 import aoc_tools as aoct
 from utils import *
-
+import itertools as it
 YEAR = 2024
 DAY = 7
 
+def compute(ops, vals):
+    res = vals[0]
+    for i, v in enumerate(vals[1:]):
+        if ops[i] == 0:
+            res = res + v
+        elif ops[i] == 1:
+            res = res * v
+        else:
+            res = int(str(res) + str(v))
+    return res
+
+def is_valid(r, vals, n_ops=2):
+    are_sums = list(it.product(list(range(n_ops)), repeat=len(vals)-1))
+    for a in are_sums:
+        if compute(a, vals) == r:
+            return True
+    return False
+
 def part_1(data):
-    return ""
+    res = 0
+    for l in data.split("\n"):
+        a, b = l.split(": ")
+        a = int(a)
+        b = b.split(" ")
+        b = [int(e) for e in b]
+        if is_valid(a, b):
+            res += a
+    return res
 
 def part_2(data):
-    return ""
+    res = 0
+    for l in data.split("\n"):
+        a, b = l.split(": ")
+        a = int(a)
+        b = b.split(" ")
+        b = [int(e) for e in b]
+        if is_valid(a, b, 3):
+            res += a
+    return res
 
 if __name__ == "__main__": 
     data = aoct.get_input(YEAR, DAY)
