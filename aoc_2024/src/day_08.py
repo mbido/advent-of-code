0ba477e512a71b5f7e52a7ba2bd3e15661b3b7b2
mbido <ma.bidault39@gmail.com>
2024-12-08 09:37:56 +0100

    mini refactor

--- aoc_2024/src/day_08.py
+++ aoc_2024/src/day_08.py
@@ -30,7 +30,7 @@ def compute_locations(grid1):
 
 def part_1(data):
     grid1 = [list(l) for l in data.split("\n")]
-    grid2 = copy.deepcopy(grid1)
+    grid2 = [[0 for _ in range(len(grid1[0]))] for _ in grid1]
     locations = compute_locations(grid1);
     
     for c in locations:
@@ -40,35 +40,31 @@ def part_1(data):
                     continue
                 diff_x = a[1] - b[1]
                 diff_y = a[0] - b[0]
-                set_grid(a[1] + diff_x, a[0] + diff_y, grid2, "#")
-    res = 0
-    for l in grid2:
-        res += l.count("#")
-    return res
+                set_grid(a[1] + diff_x, a[0] + diff_y, grid2, 1)
+
+    return sum([sum(l) for l in grid2])
 
 def part_2(data):
     grid1 = [list(l) for l in data.split("\n")]
-    grid2 = copy.deepcopy(grid1)
-    locations = compute_locations(grid1);
+    grid2 = [[0 for _ in range(len(grid1[0]))] for _ in grid1]
+    locations = compute_locations(grid1)
     
     for c in locations:
         for a in locations[c]:
             for b in locations[c]:
                 if a == b:
-                    set_grid(a[1], a[0], grid2, "#")
+                    set_grid(a[1], a[0], grid2, 1)
                     continue
                 diff_x = a[1] - b[1]
                 diff_y = a[0] - b[0]
                 x = a[1] + diff_x
                 y = a[0] + diff_y
                 while 0 <= x < len(grid2[0]) and 0 <= y < len(grid2):
-                    set_grid(x, y, grid2, "#")
+                    set_grid(x, y, grid2, 1)
                     x += diff_x
                     y += diff_y
-    res = 0
-    for l in grid2:
-        res += l.count("#")
-    return res
+                    
+    return sum([sum(l) for l in grid2])
 
 if __name__ == "__main__": 
     data = aoct.get_input(YEAR, DAY)
