734218fdc5569b8166fa7b26582f5f860f93daa6
mbido <ma.bidault39@gmail.com>
2024-12-08 06:44:56 +0100

    2024 day 8

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 My participation at the advent of code in python
 
-![Statistiques AOC](https://aoc-stats.vercel.app/api/card?username=mbido&totalStars=126&currentYearStars=14&currentDay76&completedDays=7&currentYear=2024)
+![Statistiques AOC](https://aoc-stats.vercel.app/api/card?username=mbido&totalStars=128&currentYearStars=16&currentDay=8&completedDays=8&currentYear=2024)
--- aoc_2024/src/day_08.py
+++ aoc_2024/src/day_08.py
@@ -7,11 +7,68 @@ from utils import *
 YEAR = 2024
 DAY = 8
 
+def print_grid(grid):
+    for r in grid:
+        p = ""
+        for c in r:
+            p += c
+        print(p)
+    print()
+    
+def compute_locations(grid1):
+    locations = {}
+    for i, r in enumerate(grid1):
+        for j, c in enumerate(r):
+            if c == ".":
+                continue
+            if c in locations:
+                locations[c].add((i, j))
+            else:
+                locations[c] = set()
+                locations[c].add((i,j))
+    return locations
+
 def part_1(data):
-    return ""
+    grid1 = [list(l) for l in data.split("\n")]
+    grid2 = copy.deepcopy(grid1)
+    locations = compute_locations(grid1);
+    
+    for c in locations:
+        for a in locations[c]:
+            for b in locations[c]:
+                if a == b:
+                    continue
+                diff_x = a[1] - b[1]
+                diff_y = a[0] - b[0]
+                set_grid(a[1] + diff_x, a[0] + diff_y, grid2, "#")
+    res = 0
+    for l in grid2:
+        res += l.count("#")
+    return res
 
 def part_2(data):
-    return ""
+    grid1 = [list(l) for l in data.split("\n")]
+    grid2 = copy.deepcopy(grid1)
+    locations = compute_locations(grid1);
+    
+    for c in locations:
+        for a in locations[c]:
+            for b in locations[c]:
+                if a == b:
+                    set_grid(a[1], a[0], grid2, "#")
+                    continue
+                diff_x = a[1] - b[1]
+                diff_y = a[0] - b[0]
+                x = a[1] + diff_x
+                y = a[0] + diff_y
+                while 0 <= x < len(grid2[0]) and 0 <= y < len(grid2):
+                    set_grid(x, y, grid2, "#")
+                    x += diff_x
+                    y += diff_y
+    res = 0
+    for l in grid2:
+        res += l.count("#")
+    return res
 
 if __name__ == "__main__": 
     data = aoct.get_input(YEAR, DAY)
