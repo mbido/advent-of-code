7c1c7a88bef5e82834033ca67fd5ad86a887f2da
mbido <ma.bidault39@gmail.com>
2024-12-11 06:50:01 +0100

    2024 day 11

--- aoc_2024/src/day_11.py
+++ aoc_2024/src/day_11.py
@@ -7,11 +7,46 @@ from utils import *
 YEAR = 2024
 DAY = 11
 
+def blink(n):
+    if n == 0:
+        return [1]
+    str_n = str(n)
+    l = len(str_n)
+    if l % 2 == 0:
+        return [int(str_n[:l//2]), int(str_n[l//2:])]
+    return [n * 2024]
+
+map = {}
+
+def recursive(n, repeat):
+    global map
+    if repeat == 1:
+        return len(blink(n))
+    key = (n, repeat)
+    if key in map:
+        return map[key]
+    t = blink(n)
+    res = sum(recursive(l, repeat - 1) for l in t)
+    map[key] = res
+    return res
+
 def part_1(data):
-    return ""
+    global map
+    tab = [int(n) for n in data.split(" ")]
+    res = 0
+    for n in tab:
+        map = {}
+        res += recursive(n, 25)
+    return res
 
 def part_2(data):
-    return ""
+    global map
+    tab = [int(n) for n in data.split(" ")]
+    res = 0
+    for n in tab:
+        map = {}
+        res += recursive(n, 75)
+    return res
 
 if __name__ == "__main__": 
     data = aoct.get_input(YEAR, DAY)
