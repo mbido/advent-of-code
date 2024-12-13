51025f741da0a601b29df456359346324dfde7c6
mbido <ma.bidault39@gmail.com>
2024-12-13 10:39:23 +0100

    2023 day 2

--- aoc_2023/src/day_02.py
+++ aoc_2023/src/day_02.py
@@ -7,17 +7,38 @@ from utils import *
 YEAR = 2023
 DAY = 2
 
+def parse(cols, g):
+    c = [0, 0, 0]
+    for n, j in re.findall(r'([0-9]+) ([a-z]+)', g):
+        c[cols.index(j)] += int(n)
+    return c
+
 def part_1(data):
-    #data = as_grid(data)
-    for l in data.split("\n"):
-        l = nums(l)
-    return ""
+    cols = ["blue", "red", "green"]
+    lim = [14, 12, 13]
+    res  = 0
+    for i, l in enumerate(data.split("\n")):
+        l = l.split(";")
+        valid = True
+        for g in l:
+            c = parse(cols, g)
+            if sum([int(a <= b) for a, b in zip(c, lim)]) != 3:
+                valid = False
+        if valid :
+            res += i + 1
+    return res
 
 def part_2(data):
-    #data = as_grid(data)
+    cols = ["blue", "red", "green"]
+    res  = 0
     for l in data.split("\n"):
-        l = nums(l)
-    return ""
+        l = l.split(";")
+        cmin = [0, 0, 0]
+        for g in l:
+            c = parse(cols, g)
+            cmin = [max(c[i], cmin[i]) for i in range(3)]
+        res += reduce(lambda a, b : a * b, cmin)
+    return res
 
 if __name__ == "__main__": 
     data = aoct.get_input(YEAR, DAY)
