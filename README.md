55a588aea56dc32589946dc438f48199eec7afaa
mbido <ma.bidault39@gmail.com>
2024-12-19 10:13:22 +0100

    2024 day 19

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 My participation at the advent of code in python
 
-![Statistiques AOC](https://aoc-stats.vercel.app/api/card?username=mbido&totalStars=151&currentYearStars=36&currentDay=18&completedDays=18&currentYear=2024)
+![Statistiques AOC](https://aoc-stats.vercel.app/api/card?username=mbido&totalStars=153&currentYearStars=38&currentDay=19&completedDays=19&currentYear=2024)
--- aoc_2024/src/day_19.py
+++ aoc_2024/src/day_19.py
@@ -8,16 +8,32 @@ YEAR = 2024
 DAY = 19
 
 def part_1(data):
-    #data = as_grid(data)
-    for l in data.split("\n"):
-        l = nums(l)
-    return ""
+    s1, s2 = data.split("\n\n")
+    r = "^(" + "|".join(s1.split(", ")) + ")+$"
+    return len([line for line in s2.split("\n") if re.match(r, line)])
+
+mem = {}
+
+def possible(patterns, line):
+    if line in mem:
+        return mem[line]
+    res = 0
+    for p in patterns:
+        if p == line:
+            res += 1
+        elif line.startswith(p):
+            res += possible(patterns, line[len(p):])
+    mem[line] = res
+    return res
 
 def part_2(data):
-    #data = as_grid(data)
-    for l in data.split("\n"):
-        l = nums(l)
-    return ""
+    s1, s2 = data.split("\n\n")
+    s1 = s1.split(", ")
+    s2 = s2.split("\n")
+    res = 0
+    for l in s2:
+        res += possible(s1, l)
+    return res
 
 if __name__ == "__main__": 
     data = aoct.get_input(YEAR, DAY)
