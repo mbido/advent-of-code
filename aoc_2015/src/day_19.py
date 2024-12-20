71921853ba79f768ac7856575d8e151993c19f8c
mbido <ma.bidault39@gmail.com>
2024-12-20 15:41:38 +0100

    2015 day 19 part 1

--- aoc_2015/src/day_19.py
+++ aoc_2015/src/day_19.py
@@ -8,11 +8,54 @@ YEAR = 2015
 DAY = 19
 
 data = aoct.get_input(YEAR, DAY)
+# print(data)
+s1, s2 = data.split("\n\n")
 
-res = 0
+# s1 = """e => H
+# e => O
+# H => HO
+# H => OH
+# O => HH"""
 
-#data = as_grid(data)
-for l in data.split("\n"):
-    l = nums(l)
+# s2 = "HOH"
 
-print(res)
+transforms = re.findall(r'([A-Z]?[a-z]?) \=\> ([A-Za-z]+)', s1)
+tr = {}
+for k, v in transforms:
+    if not k in tr:
+        tr[k] = [v]
+    else:
+        tr[k].append(v)
+        
+ml = re.findall(r'([A-Z][a-z]?)', s2)
+
+s = set()
+for i, a in enumerate(ml):
+    if a not in tr:
+        continue
+    for t in tr[a]:
+        tt = re.findall(r'([A-Z][a-z]?)', t)
+        mlt = ml[:i] + tt + ml[i + 1:]
+        s.add("".join(mlt))
+
+print(len(s))
+
+rvt = {}
+for k, v in tr.items():
+    for e in v:
+        if e not in rvt:
+            rvt[e] = [k]
+        else:
+            rvt[e].append(k)
+print(rvt)
+
+# cache = {}
+# def go_to_e(rvt, ml):
+#     if ml == "e":
+#         return 0
+#     for i, a in enumerate(ml):
+#         if a not in rvt:
+#             continue
+#         for t in rvt[a]:
+#             tt = re.findall(r'([A-Z][a-z]?)', t)
+#             mlt = ml[:i] + tt + ml[i + 1:]
