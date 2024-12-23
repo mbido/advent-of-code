cf4900b6f52f90d6d73def7d37ac4a7abcab2a01
mbido <ma.bidault39@gmail.com>
2024-12-23 10:58:08 +0100

    template update

--- aoc_2024/src/day_24.py
+++ aoc_2024/src/day_24.py
@@ -15,7 +15,9 @@ res = 0
 # V = list(set(re.findall(r'[a-z]{2}', data)))
 # V_T = {V[i] : i for i in range(len(V))}
 # edges = [(V_T[a], V_T[b]) for a, b in re.findall(r'([a-z]{2})\-([a-z]{2})', data)]
-# G = ig.Graph(edges=edges)
+# G = ig.Graph(edges=edges, directed=False)
+# # edges = [(V_T[a], V_T[b], int(c)) for a, b, c in re.findall(r'([a-z]{2})\-([a-z]{2})\-(\d+)', data)]
+# # G = ig.Graph(edges=[(s,t) for s,t,_ in edges], directed=False, edge_attrs={"weight": [w for _,_,w in edges]})
 
 #data = as_grid(data)
 for l in data.split("\n"):
