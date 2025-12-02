2bbc3a3144d927802e998482f12cc0ecc64b6b06
mbido <ma.bidault39@gmail.com>
2025-12-02 06:23:17 +0100

    aoc 2025 day 2

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 My participation at the advent of code.
 
-![Statistiques AOC](https://aoc-stats.vercel.app/api/card?username=mbido&totalStars=187&currentYearStars=2&currentDay=1&completedDays=1&currentYear=2025)
+![Statistiques AOC](https://aoc-stats.vercel.app/api/card?username=mbido&totalStars=189&currentYearStars=4&currentDay=2&completedDays=2&currentYear=2025)
--- aoc_2025/src/day_02.py
+++ aoc_2025/src/day_02.py
@@ -1,6 +1,7 @@
 import os
 import sys
-sys.path.append(os.path.abspath(os.path.join(__file__, '..', '..', '..')))
+
+sys.path.append(os.path.abspath(os.path.join(__file__, "..", "..", "..")))
 import aoc_tools as aoct
 from utils import *
 
@@ -19,8 +20,38 @@ res = 0
 # edges = [(V_T[a], V_T[b], int(c)) for a, b, c in re.findall(r'([a-z]{2})\-([a-z]{2})\-(\d+)', data)]
 # G = ig.Graph(edges=[(s,t) for s,t,_ in edges], directed=False, edge_attrs={"weight": [w for _,_,w in edges]})
 
-#data = as_grid(data)
-for l in data.split("\n"):
-    l = nums(l)
+# data = as_grid(data)
+# for l in data.split("\n"):
+#     l = nums(l)
 
+# data = """11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"""
+
+ranges = [nums(p) for p in data.split(",")]
+for a, b in ranges:
+    for i in range(a, b + 1):
+        si = str(i)
+        l = len(si) // 2
+        if si[:l] == si[l:]:
+            res += i
+print(res)
+
+
+def parse_size_n(s, n):
+    res = []
+    for i in range((len(s) + n - 1) // n):
+        res.append(s[i * n : (i + 1) * n])
+    return res
+
+
+res = 0
+ranges = [nums(p) for p in data.split(",")]
+for a, b in ranges:
+    for i in range(a, b + 1):
+        si = str(i)
+        for l in range(1, len(si)):
+            parsed = parse_size_n(si, l)
+            b = [elt == parsed[0] for elt in parsed]
+            if all(b):
+                res += i
+                break
 print(res)
