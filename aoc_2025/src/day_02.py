4cca20a76afc34aa00d80f12f72ec5292294ad17
mbido <ma.bidault39@gmail.com>
2025-12-02 07:26:09 +0100

    osef

--- aoc_2025/src/day_02.py
+++ aoc_2025/src/day_02.py
@@ -26,6 +26,7 @@ res = 0
 
 # data = """11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124"""
 
+
 ranges = [nums(p) for p in data.split(",")]
 for a, b in ranges:
     for i in range(a, b + 1):
@@ -48,7 +49,7 @@ ranges = [nums(p) for p in data.split(",")]
 for a, b in ranges:
     for i in range(a, b + 1):
         si = str(i)
-        for l in range(1, len(si) // 2):
+        for l in range(1, len(si) // 2 + 1):
             parsed = parse_size_n(si, l)
             b = [elt == parsed[0] for elt in parsed]
             if all(b):
