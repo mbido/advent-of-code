31553848a6ffd7bca68babdd14799f8a0c2dcbde
mbido <ma.bidault39@gmail.com>
2025-12-05 07:07:43 +0100

    Mise à jour des statistiques AOC et amélioration de la fonction part_2 pour le jour 3

--- aoc_2017/src/day_03.py
+++ aoc_2017/src/day_03.py
@@ -1,11 +1,13 @@
 import os
 import sys
-sys.path.append(os.path.abspath(os.path.join(__file__, '..', '..', '..')))
+
+sys.path.append(os.path.abspath(os.path.join(__file__, "..", "..", "..")))
 import aoc_tools as aoct
 from utils import *
 
 YEAR = 2017
 DAY = 3
+
 
 def part_1(data):
     x = int(data)
@@ -15,12 +17,39 @@ def part_1(data):
     d = min([abs(a - x) for a in c])
     return 2 * k - d
 
-def part_2(data):
-    for l in data.split("\n"):
-        l = nums(l)
-    return ""
 
-if __name__ == "__main__": 
+def part_2(data):
+    data = int(data)
+    # data = 56
+    grid = {(0, 0): 1}
+    dirs = [(0, 1), (-1, 0), (0, -1), (1, 0)]
+    di = 0
+    coord = (0, 0)
+    while grid[coord] <= data:
+        while True:
+            coord = (coord[0] + dirs[di][0], coord[1] + dirs[di][1])
+            nw = [
+                grid[key]
+                for x, y in adj8
+                if (key := (coord[0] + x, coord[1] + y)) in grid
+            ]
+            # print(coord, sum(nw), nw)
+            snw = sum(nw)
+            if snw > data:
+                return snw
+            grid[coord] = sum(nw)
+
+            if (
+                coord[0] + dirs[(di + 1) % 4][0],
+                coord[1] + dirs[(di + 1) % 4][1],
+            ) not in grid:
+                break
+        di = (di + 1) % 4
+
+    return False
+
+
+if __name__ == "__main__":
     data = aoct.get_input(YEAR, DAY)
     aoct.submit_answer(YEAR, DAY, part_1(data), send=False)
     aoct.submit_answer(YEAR, DAY, part_2(data), level=2, send=False)
